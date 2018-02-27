1601044c18b3b1a75d952d15e3d3e0019b1a2616
Vitaly Shvetsov <nosferatu2995@mail.ru>
2018-02-27 10:10:55 +0700

    update install scrypt.

--- Electrum_Wallet_install.sh
+++ Electrum_Wallet_install.sh
@@ -15,6 +15,14 @@ cd electrumx-wallet-tor
 sudo apt-get install git pyqt4-dev-tools python-pip python-dev python-slowaes
 sudo python3.5 -m pip install pyasn1 pyasn1-modules pbkdf2 tlslite qrcode
 
+sudo add-apt-repository ppa:webupd8team/tor-browser
+sudo apt-get update
+sudo apt-get install tor-browser -y
+
+sudo cp -r /var/lib/tor /var/lib/tor2
+
+sudo cp torrc_el /etc/tor
+
 pyrcc4 icons.qrc -o gui/qt/icons_rc.py
 
 chmod 775 electrum-xvg
@@ -23,5 +31,6 @@ sudo cp -r .electrum-xvg-tor $HOME
 
 sudo chown -R $USER:$USER $HOME/.electrum-xvg-tor/
 
+sudo tor -f /etc/tor/torrc_el  &
 
 ./electrum-xvg
