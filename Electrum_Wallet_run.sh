1c00870dddccebd56a99f6b18071416ccbc9db5d
Vitaly Shvetsov <nosferatu2995@mail.ru>
2018-03-02 10:49:56 +0700

    change logo  & add run scrypt.

---
+++ Electrum_Wallet_run.sh
@@ -0,0 +1,19 @@
+sudo tor -f /etc/tor/torrc_el --RunAsDaemon 1
+
+#############################
+########## WARNING ##########
+#############################
+
+# Save  your unique key for access to blockchain'
+
+################################
+
+sudo sed -n '1p' tor/hostname
+
+################################
+
+# Use upper line for  connect to server'
+
+
+./electrum-xvg
+
