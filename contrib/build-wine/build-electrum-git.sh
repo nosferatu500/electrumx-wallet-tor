8d898b323041729c17b5a60921b78ddc04d8ce5a
Vitaly Shvetsov <vshvetsov@allmax.team>
2018-03-06 20:31:06 +0700

    Add scripts.

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -1,69 +1,88 @@
 #!/bin/bash
 
-# You probably need to update only this link
-ELECTRUM_GIT_URL=git://github.com/nosferatu500/electrumx-wallet-tor.git
-BRANCH=master
-NAME_ROOT=electrum-xvg-tor
+NAME_ROOT=electrum
+PYTHON_VERSION=3.5.4
 
 # These settings probably don't need any change
-export WINEPREFIX=/opt/wine-electrum
-PYHOME=c:/python26
+export WINEPREFIX=/opt/wine64
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONHASHSEED=22
+
+PYHOME=c:/python$PYTHON_VERSION
 PYTHON="wine $PYHOME/python.exe -OO -B"
+
 
 # Let's begin!
 cd `dirname $0`
 set -e
 
+mkdir -p tmp
 cd tmp
 
-if [ -d "electrum-xvg-tor-git" ]; then
-    # GIT repository found, update it
-    echo "Pull"
+for repo in electrum electrum-locale electrum-icons; do
+    if [ -d $repo ]; then
+	cd $repo
+	git pull
+	git checkout master
+	cd ..
+    else
+	URL=https://github.com/spesmilo/$repo.git
+	git clone -b master $URL $repo
+    fi
+done
 
-    cd electrum-xvg-tor-git
-    git pull
-    cd ..
+pushd electrum-locale
+for i in ./locale/*; do
+    dir=$i/LC_MESSAGES
+    mkdir -p $dir
+    msgfmt --output-file=$dir/electrum.mo $i/electrum.po || true
+done
+popd
 
-else
-    # GIT repository not found, clone it
-    echo "Clone"
-
-    git clone -b $BRANCH $ELECTRUM_GIT_URL electrum-xvg-tor-git
+pushd electrum
+if [ ! -z "$1" ]; then
+    git checkout $1
 fi
 
-cd electrum-xvg-tor-git
-COMMIT_HASH=`git rev-parse HEAD | awk '{ print substr($1, 0, 11) }'`
-echo "Last commit: $COMMIT_HASH"
-cd ..
+VERSION=`git describe --tags`
+echo "Last commit: $VERSION"
+find -exec touch -d '2000-11-11T11:11:11+00:00' {} +
+popd
 
+rm -rf $WINEPREFIX/drive_c/electrum
+cp -r electrum $WINEPREFIX/drive_c/electrum
+cp electrum/LICENCE .
+cp -r electrum-locale/locale $WINEPREFIX/drive_c/electrum/lib/
+cp electrum-icons/icons_rc.py $WINEPREFIX/drive_c/electrum/gui/qt/
 
-rm -rf $WINEPREFIX/drive_c/electrum-xvg-tor
-cp -r electrum-xvg-tor-git $WINEPREFIX/drive_c/electrum-xvg-tor
-cp electrum-xvg-tor-git/LICENCE .
+# Install frozen dependencies
+$PYTHON -m pip install -r ../../deterministic-build/requirements.txt
 
-# Build Qt resources
-wine $WINEPREFIX/drive_c/Python26/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum-xvg-tor-git/icons.qrc -o C:/electrum-xvg-tor-git/lib/icons_rc.py
+$PYTHON -m pip install -r ../../deterministic-build/requirements-hw.txt
 
-# Copy ZBar libraries to electrum
-#cp "$WINEPREFIX/drive_c/Program Files (x86)/ZBar/bin/"*.dll "$WINEPREFIX/drive_c/electrum-xvg-tor-git/"
+pushd $WINEPREFIX/drive_c/electrum
+$PYTHON setup.py install
+popd
 
 cd ..
 
 rm -rf dist/
 
-# For building standalone compressed EXE, run:
-$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w --onefile "C:/electrum-xvg-tor-git/electrum-xvg"
+# build standalone and portable versions
+wine "C:/python$PYTHON_VERSION/scripts/pyinstaller.exe" --noconfirm --ascii --name $NAME_ROOT-$VERSION -w deterministic.spec
 
-# For building uncompressed directory of dependencies, run:
-$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
+# set timestamps in dist, in order to make the installer reproducible
+pushd dist
+find -exec touch -d '2000-11-11T11:11:11+00:00' {} +
+popd
 
-# For building NSIS installer, run:
-wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" electrum-xvg.nsi
-#wine $WINEPREFIX/drive_c/Program\ Files\ \(x86\)/NSIS/makensis.exe electrum-xvg.nsis
+# build NSIS installer
+# $VERSION could be passed to the electrum.nsi script, but this would require some rewriting in the script iself.
+wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=$VERSION electrum.nsi
 
-DATE=`date +"%Y%m%d"`
 cd dist
-mv electrum-xvg.exe $NAME_ROOT-$DATE-$COMMIT_HASH.exe
-mv electrum-xvg $NAME_ROOT-$DATE-$COMMIT_HASH
-mv electrum-xvg-setup.exe $NAME_ROOT-$DATE-$COMMIT_HASH-setup.exe
-zip -r $NAME_ROOT-$DATE-$COMMIT_HASH.zip $NAME_ROOT-$DATE-$COMMIT_HASH
+mv electrum-setup.exe $NAME_ROOT-$VERSION-setup.exe
+cd ..
+
+echo "Done."
+md5sum dist/electrum*exe
