0e6ecef28debd7390af0ddd756a5a90807159bb9
Admin <admin@Mac-Admin.local>
2018-03-19 09:05:04 +0300

    Update .gitignore.

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ if sys.version_info[:3] < (2, 7, 0):
 data_files = []
 if platform.system() in [ 'Linux', 'FreeBSD', 'DragonFly']:
     usr_share = os.path.join(sys.prefix, "share")
-data_files += [
-    (os.path.join(usr_share, 'applications/'), ['electrum-xvg.desktop']),
-    (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum-xvg.png'])
-]
+    data_files += [
+        (os.path.join(usr_share, 'applications/'), ['electrum-xvg.desktop']),
+        (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum-xvg.png'])
+    ]
 
 
 setup(
